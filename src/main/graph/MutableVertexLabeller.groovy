31aa03e1f64f122571083e63b974ea2cc0b3f077
sfsgagi <sfsgagi@gmail.com>
2013-03-31 00:55:53 +0100

    Better crosspoint labelling on graph

--- src/main/graph/MutableVertexLabeller.groovy
+++ src/main/graph/MutableVertexLabeller.groovy
@@ -4,7 +4,10 @@ import org.apache.commons.collections15.Transformer;
 
 class MutableVertexLabeller<V> implements Transformer<V,String> {
 
-    public String transform(V v) {
-		return "$v.name"
-	}
+  public String transform(V v) {
+    if("$v.name".contains("\u00a9"))
+      return "\u00a9"
+    else 
+      return "$v.name"
+  }
 }
